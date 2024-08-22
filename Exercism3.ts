bb6ab3e11492d29a15e64295c598815514271b7b
xchaarles <carlosbp2608@gmail.com>
2024-08-21 19:31:48 -0500

    tercer ejercicio de exercism

---
+++ Exercism3.ts
@@ -0,0 +1,17 @@
+export const colorCode = (color : string) => {
+    return COLORS.indexOf(color)
+  }
+  
+  export const COLORS = [
+    'negro',
+    'marron',
+    'rojo',
+    'amarillo',
+    'naranja',
+    'verde',
+    'azul',
+    'violeta',
+    'gris',
+    'blanco',
+  ]
+  
